29865bf7be36815372742c946779905f3a222870
Vagiaki <danaivag@outlook.com>
2022-07-12 01:13:54 +0200

    handle case with 0 permutations

--- Limix_QTL/post_processing/multTestCorrect_PipeLine.R
+++ Limix_QTL/post_processing/multTestCorrect_PipeLine.R
@@ -1,5 +1,7 @@
 library("qvalue")
 library("multtest")
+library(stringr)
+
 args = commandArgs(trailingOnly=TRUE)
 
 # test if there is at least one argument: if not, return an error
@@ -11,6 +13,8 @@ if (length(args)<3 || length(args)>4) {
 folder = args[1]
 folderOut = args[2]
 fdrLevel = as.numeric(args[3])
+permutations = args[4]
+
 
 filterFile= NULL
 if(length(args)==4){
@@ -24,11 +28,11 @@ qtlResults["QTL"] <- paste(qtlResults$feature_id,qtlResults$snp_id,sep = "-")
 
 #Check for double QTLs.
 if(length(unique(qtlResults$QTL)) != nrow(qtlResults)){
-	qtlResults <- qtlResults[-which(duplicated(qtlResults$QTL)),]
+				  qtlResults <- qtlResults[-which(duplicated(qtlResults$QTL)),]
 }
 #Check for double genes.
 if(length(unique(qtlResults$feature_id)) != nrow(qtlResults)){
-	qtlResults <- qtlResults[-which(duplicated(qtlResults$feature_id)),]
+					 qtlResults <- qtlResults[-which(duplicated(qtlResults$feature_id)),]
 }
 
 #Filtering to a subset of the genes.
@@ -37,15 +41,17 @@ if(!is.null(filterFile)){
 	qtlResults <- qtlResults[which(qtlResults$feature_id %in% relevantGenes),]
 }
 
-qtlResults$global_corrected_pValue <- qvalue(qtlResults$empirical_feature_p_value)$qvalues
-qtlResults$global_corrected_pValue_BH <- multtest::mt.rawp2adjp(qtlResults$empirical_feature_p_value,proc = "BH")$adjp[,2]
-qtlResults$global_corrected_pValue_BF <- multtest::mt.rawp2adjp(qtlResults$empirical_feature_p_value,proc = "Bonferroni")$adjp[,2]
+pval_column <- ifelse(permutations, "empirical_feature_p_value", "p_value")
+
+qtlResults$global_corrected_pValue <- qvalue(qtlResults[pval_column])$qvalues
+qtlResults$global_corrected_pValue_BH <- multtest::mt.rawp2adjp(qtlResults[pval_column],proc = "BH")$adjp[,2]
+qtlResults$global_corrected_pValue_BF <- multtest::mt.rawp2adjp(qtlResults[pval_column],proc = "Bonferroni")$adjp[,2]
 
 ##To extract data from the full file.
-minimal_p <- max(qtlResults$empirical_feature_p_value[which(qtlResults$global_corrected_pValue<fdrLevel)])
+minimal_p <- max(qtlResults[pval_column][which(qtlResults$global_corrected_pValue<fdrLevel)])
 
 write.table(qtlResults, paste(folderOut,"/top_qtl_results_all_FDR.txt",sep=""), quote = F, row.names=F, sep="\t" )
-write.table(qtlResults[qtlResults$global_corrected_pValue<fdrLevel,], paste(folderOut,"/top_qtl_results_all_FDR",fdrLevel,".txt",sep=""), quote = F, row.names=F, sep="\t" )
+write.table(qtlResults[qtlResults$global_corrected_pValue<fdrLevel,], paste0(folderOut, "/top_qtl_results_all_FDR", str_replace(fdrLevel, "\\.", ""), ".txt"), quote = F, row.names=F, sep="\t" )
 
 qtlResultsAll <- read.delim(paste(folder,"/qtl_results_all.txt",sep=""), as.is=T)
 qtlResultsAll["QTL"] <- paste(qtlResultsAll$feature_id,qtlResultsAll$snp_id,sep = "-")
@@ -54,12 +60,12 @@ qtlResultsAll <- qtlResultsAll[order(qtlResultsAll$empirical_feature_p_value, qt
 qtlResultsAll["QTL"] <- paste(qtlResultsAll$feature_id,qtlResultsAll$snp_id,sep = "-")
 #Check for double QTLs.
 if(length(unique(qtlResultsAll$QTL)) != nrow(qtlResultsAll)){
-	qtlResultsAll <- qtlResultsAll[-which(duplicated(qtlResultsAll$QTL)),]
+				     qtlResultsAll <- qtlResultsAll[-which(duplicated(qtlResultsAll$QTL)),]
 }
 
 if(!is.null(filterFile)){
 	qtlResultsAll <- qtlResultsAll[which(qtlResultsAll$feature_id %in% relevantGenes),]
 }
 
-write.table(qtlResultsAll[qtlResultsAll$empirical_feature_p_value<=minimal_p,], paste(folderOut,"/qtl_results_all_FDR",fdrLevel,".txt",sep=""), quote = F, row.names=F, sep="\t" )
+write.table(qtlResultsAll[qtlResultsAll$empirical_feature_p_value<=minimal_p,], paste0(folderOut, "/qtl_results_all_FDR", str_replace(fdrLevel, "\\.", ""), ".txt"), quote = F, row.names=F, sep="\t" )
 
